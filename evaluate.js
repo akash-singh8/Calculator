cef2f3eeac789963fb6bd573d26117241ea2c574
Akash Kumar Singh <dipu64601@gmail.com>
2022-11-13 12:13:08 +0530

    Adding Functionallity

---
+++ evaluate.js
@@ -0,0 +1,47 @@
+let expression = document.getElementById("expression");
+let buttons = document.querySelectorAll("button");
+
+let Ans = 0;
+let flag = false;
+
+function evaluate() {
+  if (expression.value.length) {
+    try {
+      Ans = eval(expression.value);
+      expression.style.color = "#939393";
+      expression.value += `=${Ans}`;
+      flag = true;
+    } catch (err) {
+      console.log(err);
+      alert("Invalid Expression : " + expression.value);
+      expression.value = "";
+    }
+  }
+}
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (flag) {
+      expression.value = "";
+      expression.style.color = "white";
+      flag = false;
+    }
+    let txt = btn.innerText;
+    if (txt === "=") {
+      evaluate();
+    } else if (txt === "del") {
+      expression.value = expression.value.substr(
+        0,
+        expression.value.length - 1
+      );
+    } else if (txt === "C") {
+      expression.value = "";
+    } else if (txt === "Ans") {
+      expression.value += Ans;
+    } else if (txt === "^") {
+      expression.value += "**";
+    } else {
+      expression.value += txt;
+    }
+  });
+});
